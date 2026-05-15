223f541265d78f883abba5d6aa300de9a22262e1
Samrat <md721023@gmail.com>
2026-05-16 03:55:13 +0600

    blog running

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,8 +1,15 @@
+import BlogCard from "./BlogCard";
+import blogs from "../../blogCard.json";
+
 export default function Blogs() {
   return (
     <section className="">
-      <div className="grid grid-cols-3 max-w-360 mx-auto p-5  ">
-        <div className="col-span-2"></div>
+      <div className="grid lg:grid-cols-3 max-w-360 mx-auto p-5">
+        <div className="col-span-2 grid  gap-5">
+          {blogs.map((blog) => (
+            <BlogCard key={blog.title} blog={blog} />
+          ))}
+        </div>
         <div></div>
       </div>
     </section>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
+import Blogs from "../components/Blogs";
 import Hero from "../components/Hero";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Blogs />
     </main>
   );
 }
